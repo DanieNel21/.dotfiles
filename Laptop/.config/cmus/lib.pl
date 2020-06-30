4fe407d70c39fb4ac19925c2d9226b881baa2c16
DanieNel21 <u18028277@tuks.co.za>
2020-06-30 17:04:44 +0200

    Dotfiles github

---
+++ Laptop/.config/cmus/lib.pl
@@ -0,0 +1,3 @@
+/home/danie/Music/Studying/Fantasy Music - Daydream Mix-XbS3tPO9sUs.opus
+/home/danie/Music/Studying/RPG Playlist - Peaceful Music-4ceowgHn8BE.opus
+/home/danie/Music/Studying/The Witcher 'Geralt's Story' (A Relaxing Music Compilation) (Witcher 1, 2 & 3)-8JYJZZWyz8A.opus
